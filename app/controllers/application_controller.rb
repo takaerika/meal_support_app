0b1dcb05b0dd5b4ad49dd5ad6218ffac30aae280
takaerika <erika.nemoto12@gmail.com>
2025-08-25 18:15:31 +0900

    ユーザー管理機能のパラメーター設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_in_path_for(resource)
+    resource.supporter? ? supporter_home_path : root_path
+  end
+  
   private
-
   def production?
     Rails.env.production?
   end
@@ -16,5 +20,12 @@ class ApplicationController < ActionController::Base
       ActiveSupport::SecurityUtils.secure_compare(u, user) &
         ActiveSupport::SecurityUtils.secure_compare(p, pass)
     end
+  end
+
+  protected
+  def configure_permitted_parameters
+    keys = %i[first_name last_name role]
+    devise_parameter_sanitizer.permit(:sign_up, keys: keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: keys)
   end
 end
