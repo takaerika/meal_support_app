0f39c5071b93c6943eeb364ba7caffe87d6c0e80
takaerika <erika.nemoto12@gmail.com>
2025-08-25 12:12:08 +0900

    Basic認証導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
+
+  private
+
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    user = ENV["BASIC_AUTH_USER"]
+    pass = ENV["BASIC_AUTH_PASSWORD"]
+    return unless user.present? && pass.present?
+
+    authenticate_or_request_with_http_basic do |u, p|
+      ActiveSupport::SecurityUtils.secure_compare(u, user) &
+        ActiveSupport::SecurityUtils.secure_compare(p, pass)
+    end
+  end
 end
