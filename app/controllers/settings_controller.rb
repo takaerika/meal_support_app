ac52bf373aa89ea02d277d367a9c065328c4409e
takaerika <erika.nemoto12@gmail.com>
2025-08-26 11:25:58 +0900

    招待機能コントローラー設定

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,17 +3,10 @@ class SettingsController < ApplicationController
 
   def show
     @user = current_user
-
     if @user.patient?
-      # 連携中サポーター（1人想定の最小実装）
-      @supporter = if defined?(SupportLink)
-        SupportLink.includes(:supporter).find_by(patient_id: @user.id)&.supporter
-      end
+      @supporter = SupportLink.includes(:supporter).find_by(patient_id: @user.id)&.supporter
     else
-      # サポーターの自分の招待コード
-      @invite_code = if defined?(InviteCode)
-        InviteCode.find_by(supporter_id: @user.id)
-      end
+      @invite_code = InviteCode.find_by(supporter_id: @user.id)
     end
   end
 end
