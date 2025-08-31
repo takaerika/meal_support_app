2d7fe2acb80e31d1254bff2a7f04937f7c7ddcd9
takaerika <erika.nemoto12@gmail.com>
2025-08-31 12:36:20 +0900

    UI調整のための編集

--- app/controllers/supporter/patients_controller.rb
+++ app/controllers/supporter/patients_controller.rb
@@ -1,21 +1,17 @@
-class Supporter::PatientsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :require_supporter!
+class Supporter::PatientsController < Supporter::BaseController
   before_action :set_patient
 
   def show
     today = Date.today
     year  = params[:year].presence&.to_i || today.year
     month = params[:month].presence&.to_i || today.month
-    @month = Date.new(year, month, 1)
-
+    @month      = Date.new(year, month, 1)
     @prev_month = @month << 1
     @next_month = @month >> 1
     @first_wday = @month.wday
     @last_day   = (@month.next_month - 1).day
 
-    # 指定日の食事記録一覧
-    @date = params[:date].present? ? Date.parse(params[:date]) : today
+    @date    = params[:date].present? ? Date.parse(params[:date]) : today
     @records = @patient.meal_records.where(eaten_on: @date).order(:slot)
   end
 
@@ -23,9 +19,5 @@ class Supporter::PatientsController < ApplicationController
 
   def set_patient
     @patient = current_user.patients.find(params[:id])
-  end
-
-  def require_supporter!
-    redirect_to root_path, alert: "サポーターのみ利用できます" unless current_user.supporter?
   end
 end
