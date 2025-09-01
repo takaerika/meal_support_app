4c3508e8ae7cd8a2729d5f0b2dd14ade9a834c56
takaerika <erika.nemoto12@gmail.com>
2025-09-01 10:24:20 +0900

    roleがうまく登録できないバグ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    added = %i[last_name first_name last_name_kana first_name_kana]
+    added = %i[role last_name first_name last_name_kana first_name_kana]
     devise_parameter_sanitizer.permit(:sign_up,        keys: added)
     devise_parameter_sanitizer.permit(:account_update, keys: added)
   end
