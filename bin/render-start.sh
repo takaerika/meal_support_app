2f89603aba54a18a75ffff91ceda6ae6706143a3
takaerika <erika.nemoto12@gmail.com>
2025-09-01 15:25:43 +0900

    users.last_meal_updated_at カラムの追加

--- bin/render-start.sh
+++ bin/render-start.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
 export RAILS_ENV=production
-
 echo "[render-start] rails db:migrate"
 bundle exec rails db:migrate
-
 echo "[render-start] boot app (puma)"
 exec bundle exec puma -C config/puma.rb
